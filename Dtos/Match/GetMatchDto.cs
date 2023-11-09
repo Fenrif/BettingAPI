f8987cf89a706185bdd979a641175903a81521c5
giann_000 <kyriaz.giannis@gmail.com>
2023-11-09 13:23:44 +0200

    Match DTOs

---
+++ Dtos/Match/GetMatchDto.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using BettingAPI.Enums;
+
+namespace BettingAPI.Dtos.Match
+{
+    public class GetMatchDto
+    {
+        public Guid ID { get; set; }
+        public string Description { get; set; } = String.Empty;
+        [DataType(DataType.Date)]
+        public DateTime MatchDate { get; set; }
+        [DataType(DataType.Time)]
+        public DateTime MatchTime { get; set; }
+        public string TeamA { get; set; } = String.Empty;
+        public string TeamB { get; set; } = String.Empty;
+        public Sport Sport { get; set; }
+    }
+}
