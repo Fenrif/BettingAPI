8486d707bdf067b548c9a987d88cfebd80836b6e
giann_000 <kyriaz.giannis@gmail.com>
2023-11-09 20:01:11 +0200

    EF Migrations MatchOdds

---
+++ Migrations/20231109170349_MatchOdds.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BettingAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class MatchOdds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MatchOdds",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MatchID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Specifier = table.Column<int>(type: "int", nullable: false),
+                    Odd = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MatchOdds", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_MatchOdds_Match_MatchID",
+                        column: x => x.MatchID,
+                        principalTable: "Match",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MatchOdds_MatchID",
+                table: "MatchOdds",
+                column: "MatchID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MatchOdds");
+        }
+    }
+}
---
+++ Migrations/20231109174038_MatchFK.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BettingAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class MatchFK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
