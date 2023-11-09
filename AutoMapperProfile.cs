e2ce417d609f9d37ac60f8b0acb7acd99be01e38
giann_000 <kyriaz.giannis@gmail.com>
2023-11-09 18:38:17 +0200

    fixes for Match

--- AutoMapperProfile.cs
+++ AutoMapperProfile.cs
@@ -13,6 +13,10 @@ namespace BettingAPI
             CreateMap<InsertMatchDto, Match>();
             CreateMap<UpdateMatchDto, Match>();
 
+
+        //     CreateMap<UpdateMatchDto, Match>()
+        // .ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null)); // Ignore mapping if source is null
+
             CreateMap<MatchOdds, GetMatchOddsDto>();
             CreateMap<InsertMatchOddsDto, MatchOdds>();
             CreateMap<UpdateMatchOddsDto, MatchOdds>();
--- Dtos/Match/InsertMatchDto.cs
+++ Dtos/Match/InsertMatchDto.cs
@@ -6,13 +6,13 @@ namespace BettingAPI.Dtos.Match
 {
     public class InsertMatchDto
     {
-        public string Description { get; set; } = String.Empty;
+        public string Description { get; set; }
         [DataType(DataType.Date)]
         public DateTime MatchDate { get; set; }
         [DataType(DataType.Time)]
         public DateTime MatchTime { get; set; }
-        public string TeamA { get; set; } = String.Empty;
-        public string TeamB { get; set; } = String.Empty;
+        public string TeamA { get; set; }
+        public string TeamB { get; set; }
         public Sport Sport { get; set; }
     }
 }
