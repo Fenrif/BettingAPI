1228c7a91c98d96f663bc251881f3c5e05eed82a
giann_000 <kyriaz.giannis@gmail.com>
2023-11-09 13:26:46 +0200

    EF Initial DB Create

---
+++ Migrations/20231108183903_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BettingAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Match",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MatchDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MatchTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TeamA = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TeamB = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Sport = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Match", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Match");
+        }
+    }
+}
